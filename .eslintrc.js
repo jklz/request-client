67d4375970308e1b5d0b2d511add0b8056bcece5
Jared Spencer <jklz@me.com>
2020-04-22 21:00:41 -0600

    start setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "@typescript-eslint/indent": [2, 2],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/array-type": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/member-delimiter-style": "off",
+        "@typescript-eslint/no-object-literal-type-assertion": "off",
+    }
+};
